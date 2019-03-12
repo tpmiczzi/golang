218f31f169ee7e9e45c15d2b3e091f487f0aa3f1
Pavlo Tula <pavlo.tula@thomsonreuters.com>
2019-03-12 17:54:06 +0200

    type

--- golang-start/src/main.go
+++ golang-start/src/main.go
@@ -5,5 +5,19 @@ import "fmt"
 // this is a comment
 
 func main() {
-	fmt.Println("Hello World")
+	var x string = "Hello World"
+	fmt.Println(x)
+	variables()
+}
+
+func variables() {
+	var a1 string = "1. init variables with form - var a1 string, after init "
+	fmt.Println(a1)
+	var a2 string = "2. init variables with form - var a1 string = \"\" "
+	fmt.Println(a2)
+	a3 := "3. init variables with form - a3 := \"\", compiler define type"
+	fmt.Println(a3)
+	const a4 string = "4. init const variables - const a4 string = \"\""
+	fmt.Println(a4)
+
 }
