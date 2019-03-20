a5d44482eae3b75e2d43d5cfdf107045c49f50fa
Pavlo Tula <pavlo.tula@thomsonreuters.com>
2019-03-20 16:51:24 +0200

    types

--- golang-start/src/main.go
+++ golang-start/src/main.go
@@ -8,6 +8,7 @@ func main() {
 	var x string = "Hello World"
 	fmt.Println(x)
 	variables()
+	allType()
 }
 
 func variables() {
@@ -19,5 +20,17 @@ func variables() {
 	fmt.Println(a3)
 	const a4 string = "4. init const variables - const a4 string = \"\""
 	fmt.Println(a4)
+
+	var (
+		a    = 5
+		b    = 10
+		c    = 15
+		text = "Pavel"
+	)
+	fmt.Println("amount -", a+b+c, "and name -", text)
+}
+func allType() {
+	fmt.Println("integer types - uint8, uint16, uint32, uint64, int8, int16, int32 и int64")
+	fmt.Println("unsigned integer and signed integer")
 
 }
